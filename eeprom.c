abad44b62fb7dafd3aeffa71abd668f8cd07d171
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-03-14 10:48:50 +0100

    initiate the implementation

---
+++ eeprom.c
@@ -0,0 +1,17 @@
+/*
+ * eeprom.c
+ *
+ * Created: 3/14/2026 10:38:02 AM
+ * Author: Sarve
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
